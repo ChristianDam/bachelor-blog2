a4801872f8624a6d8cc46377c6a210d8f21c5c1e
Christian Dam <christian.dam@avidlyagency.com>
2019-02-03 11:34:25 +0100

    Fixing

--- config.js
+++ config.js
@@ -1,27 +1,33 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: "https://it-bachelor.netlify.com",
+  url: "https://it-bachelor.netlify.com/",
   title: "IT Bachelor Blog",
   subtitle:
     "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  copyright: "© All rights reserved.",
   disqusShortname: "",
   postsPerPage: 4,
+  googleAnalyticsId: "UA-73379983-2",
   menu: [
     {
       label: "Articles",
       path: "/"
     },
     {
-      label: "About the project",
+      label: "About me",
       path: "/pages/about"
+    },
+    {
+      label: "Contact me",
+      path: "/pages/contacts"
     }
   ],
   author: {
     name: "Simon and Christian",
     photo: "/astronaut.jpg",
     bio:
-      "In collaboration with Danske Bank, we are writing a bachelors project on how to rethink and innovate how people experience and do authentication.",
+      "In collaboration with Danske Bank we are writing a bachelor's project on how to develop the next authentication.",
     contacts: {
       email: "#",
       telegram: "#",
